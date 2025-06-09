5b7feb2a661be9a44dda2041475d6fdd0e0b2717
olaniyi <arokoyueb11@gmail.com>
2025-06-09 07:54:51 +0100

    fixing cors error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // Enable CORS for frontend origin
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://baby-story-ai.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
